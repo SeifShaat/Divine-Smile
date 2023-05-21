3da730fa3df6fe15038dd88f1dd00cd86151e00b
SeifShaat <88462959+SeifShaat@users.noreply.github.com>
2023-05-22 01:26:25 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 apt-get update
-apt-get install -y cmake
-apt-get install -y libboost-all-dev
+apt-get install -y build-essential cmake
 apt-get install -y libopenblas-dev liblapack-dev
 pip install numpy opencv-python flask
-pip install cmake
 pip install dlib
