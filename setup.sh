878a0ad0d48d56ad86f05e7bfd692e95a75ecb2e
SeifShaat <88462959+SeifShaat@users.noreply.github.com>
2023-05-22 01:27:59 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,14 @@ apt-get update
 apt-get install -y build-essential cmake
 apt-get install -y libopenblas-dev liblapack-dev
 pip install numpy opencv-python flask
-pip install dlib
+
+# Manually install Dlib and its dependencies
+cd ~
+git clone https://github.com/davisking/dlib.git
+cd dlib
+mkdir build
+cd build
+cmake ..
+cmake --build .
+cd ..
+python setup.py install
