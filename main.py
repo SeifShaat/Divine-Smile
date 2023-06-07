44eb96a51277efeb581fb8c3e83c2e0eaab7d2b4
SeifShaat <88462959+SeifShaat@users.noreply.github.com>
2023-06-07 20:04:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def MainAccess():
-    response_data = {"message": "Bom"}
+    response_data = {"message": "Test"}
     return jsonify(response_data)
 
 # Load the Haar Cascade classifier for face detection
